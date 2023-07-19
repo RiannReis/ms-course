1aa9243916961880269c47f72e4c1de0312bd90e
Riann Silva <ooriannsilva@gmail.com>
2023-07-19 17:50:50 -0300

    Zuul timeout

--- hr-worker/src/main/java/com/riannreis/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/com/riannreis/hrworker/resources/WorkerResources.java
@@ -37,11 +37,11 @@ public class WorkerResources {
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
 		
 		
-		/*try {
+		try {
 			Thread.sleep(3000L);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-		}*/
+		}
 		
 		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
